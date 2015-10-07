af8e0b70b96031efee83296ef3e5429e3e1af614
Emil Guseynov <emil@MacBook-Air-Emil.local>
2015-10-07 23:43:10 +0300

    Task 2 Draft

---
+++ 2/main.pl
@@ -0,0 +1,126 @@
+use strict;
+use warnings;
+use POSIX qw( isdigit );
+use feature 'switch';
+
+my %priority = (
+    '+' => 1,
+    '-' => 1,
+    '*' => 2,
+    '^' => 3
+);
+
+sub get_lexems {
+    my @lexems = ();
+    my @array = split(//, $_[0]);
+    my $number = "";
+    my $prev_char = undef;
+
+    foreach (@array) {
+        my $skip = undef;
+        if ($prev_char eq '*') {
+            if ($_ eq '*') {
+                push @lexems, '^';
+            } else {
+                push @lexems, '*';
+                push @lexems, $_
+            }
+            $skip = 1
+        } elsif (isdigit($_)) {
+            $number .= $_ ;
+        } elsif ($_ ne '*') {
+            push @lexems, $_
+        }
+        if ($number) {
+            push @lexems, $number;
+            $number = ""
+        }
+        if (not $skip) {
+            $prev_char = $_;
+        } else {
+            $prev_char = undef;
+        }
+    }
+    if ($number) {
+        push @lexems, $number
+    }
+    return @lexems
+}
+
+sub convert_to_polish {
+    my @stack_num = ();
+    my @stack_op = ();
+    my @result = ();
+
+    foreach (@_) {
+        if (isdigit($_)) {
+            push @stack_num, $_
+        } else {
+            my $op_prior = $priority{$_};
+            while (@stack_op and $priority{$stack_op[-1]} >= $op_prior) {
+                print "hi\n";
+                my $cur_op = pop @stack_op;
+                #@result and push @result, (pop @stack_num) or push @result, (pop @stack_num) and push @result, (pop @stack_num);
+                if (@result) { push @result, (pop @stack_num, $cur_op)}
+                else {
+                    my $b = pop @stack_num;
+                    my $a = pop @stack_num;
+                    push @result, ($a, $b, $cur_op)
+                }
+            }
+            push @stack_op, $_
+        }
+    }
+    #print join ' ', @stack_op, "\n";
+    while (@stack_op) {
+        my $cur_op = pop @stack_op;
+        if (@result) { push @result, (pop @stack_num, $cur_op)}
+        else {
+            my $b = pop @stack_num;
+            my $a = pop @stack_num;
+            push @result, ($a, $b, $cur_op)
+        }
+    }
+    return @result;
+}
+
+sub eval_polish {
+    my @stack = ();
+    foreach (@_) {    
+        no warnings;
+        given ($_) {
+            when ('+') { push @stack, (pop @stack) + pop @stack } 
+            when ('-') { push @stack, - (pop @stack) + pop @stack }
+            when ('*') { push @stack, (pop @stack) * pop @stack }
+            when ('^') { push @stack, do { my $b = (pop @stack); my $a = pop @stack; $a ** $b } }
+            default { push @stack, $_ }  # number
+        }
+=pod
+        my $len = scalar @stack;
+        print "DEBUG: len=$len ";
+        foreach my $c (@stack) {
+            print $c, ' ';
+        }
+        print "\n";
+=cut
+    }
+    return pop @stack
+};
+
+
+sub evaluate_expr {
+    return eval_polish(convert_to_polish(get_lexems($_[0])));
+}
+
+#my $line = readline(*STDIN);
+my $line = '1+2**3-7';
+my @items = get_lexems($line);
+#print join ' ', @items, "\n";
+print join ' ', convert_to_polish(@items), "\n";
+#print eval_polish(convert_to_polish(@items));
+print evaluate_expr($line);
+=pod
+foreach (@items) {
+    print $_, ' ', $priority{$_}, "\n"
+}
+=cut
